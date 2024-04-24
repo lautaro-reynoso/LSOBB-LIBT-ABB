917bc5a24d8c1676586c5540c4e7a2a9b0347c92
Mateo521 <mateovillalva42@gmail.com>
2024-04-24 11:50:07 -0300

    lsobb prueba

--- LSOBB.h
+++ LSOBB.h
@@ -1,5 +1,4 @@
 
-
 #include "Envios.h"
 #include <String.h>
 #include <math.h>
@@ -9,13 +8,14 @@
 #endif //UNTITLED_LSO_H
 
 //  float eExMax=0.0, eExMed=0.0, eFrMax=0.0, eFrMed=0.0, aMax=0.0, aMed=0.0, bMax=0.0, bMed=0.0, celCont=0.0;
- //   int eExCant=0,eFrCant=0,aCant=0,bCant=0;
+//   int eExCant=0,eFrCant=0,aCant=0,bCant=0;
 
 char *Mayusculas(char string[]);
 typedef struct {
     Envio envios [MAX_Envios];
     int contador;
-  float eExMax, eExMed, eFrMax, eFrMed, aMax, aMed, bMax, bMed, celCont,tempa,tempb, eExCant,eFrCant,aCant,bCant,costo,costoEvoE,costoEvoF,tempe,tempef;
+    int vector_aux[MAX_Envios];
+    float eExMax, eExMed, eFrMax, eFrMed, aMax, aMed, bMax, bMed, celCont,tempa,tempb, eExCant,eFrCant,aCant,bCant,costo,costoEvoE,costoEvoF,tempe,tempef;
 }lsobb;
 
 
@@ -29,92 +29,101 @@ void initLSOBB(lsobb *lista) {
     lista->bMax = 0.0;
     lista->bMed = 0.0;
     lista->celCont = 0.0;
-    lista->eExCant = 0;
-    lista->eFrCant = 0;
+    lista->eExCant = 0.0;
+    lista->eFrCant = 0.0;
     lista->aCant = 0.0;
     lista->bCant = 0.0;
     lista->tempa = 0.0;
-      lista->tempb = 0.0;
-      lista->tempe= 0.0;
-      lista->tempef= 0.0;
+    lista->tempb = 0.0;
+    lista->tempe= 0.0;
+    lista->tempef= 0.0;
 }
 
 
 int LocalizarLSOBB(lsobb *lista, char codigo[], int *pos, int p) {
+    float inicio = -1, medio = 0, comparacion = 0;             // Limite inferior exclusivo
+    float fin = lista->contador ;   // Limite superior exclusivo
 
 
     lista->costoEvoE=0.0;
-lista->costoEvoF=0.0;
+    lista->costoEvoF=0.0;
+
+
     float temp =0.0;
-    int inicio = -1;  // Limite inferior exclusivo
-    int fin = lista->contador;  // Limite superior exclusivo
-
-    while (inicio+1 < fin) {
-            temp++;
-
-        int medio = ceil((inicio + fin)  / 2.0); // Testigo derecha, segmento mas grande izquierda
-
-        int comparacion = strcmp(lista->envios[medio].codigo, codigo);
-
-        if (comparacion == 0) {
-
-                  if(p==0){
-
-            if(lista->eExMax<temp){
-                lista->eExMax = temp;
-      }
-       lista->eExCant++;
-       lista->costoEvoE+=temp;
-       lista->tempe+=lista->costoEvoE;
-       lista->eExMed = lista->tempe/(lista->eExCant+1);
-
-       }
-
-
-
-            *pos = medio;
-            return 1; // Encontrado
-        } else if (comparacion < 0) {
-
-
-              if(p==0){
-      if(lista->eFrMax<temp){
-                lista->eFrMax = temp;
-      }
-
-       lista->eFrCant++;
-       lista->costoEvoF+=temp;
-        lista->tempef+=lista->costoEvoF;
-       lista->eFrMed = lista->tempef/(lista->eFrCant+1);
-
+    if(lista->contador==0){
+        return 0;
     }
+    while (inicio+1 < fin) {
+        temp++;
+        medio = ceil((inicio + fin) / 2.0);
 
 
+        comparacion = strcmp( lista->envios[(int)medio].codigo, codigo);
 
 
-            inicio = medio; //Limite inferior exclusivo
+        if (comparacion > 0) {
+
+            fin = medio;
+
         } else {
 
+            inicio = medio;
+
+
+            if (strcmp((*lista).envios[(int)fin-1].codigo,codigo)>0){
+                (*pos)=inicio;
+            }else{
+                (*pos)=inicio+1;
+            }
 
 
 
-            fin = medio;//Limite superior exclusivo
+
         }
+
+
     }
 
-    *pos = inicio + 1 ;
-    return 0;
+    //vector auxiliar
+    if(lista->vector_aux[(int)inicio+1] == 0){
+        temp++;
+        lista->vector_aux[(int)inicio+1]+=1;
+
+    }
+    // Posición de inserción si no se encontró el elemento
+
+    if (strcmp(lista->envios[(int)fin-1].codigo, codigo) == 0) {
+        if(p==0){
+            if(lista->eExMax<temp){
+                lista->eExMax = temp-1;
+            }
+            lista->eExCant++;
+            lista->costoEvoE+=temp;
+            lista->tempe+=lista->costoEvoE;
+            lista->eExMed = lista->tempe/(lista->eExCant);
+        }
+        *pos = (int)fin-1;
+        return 1; // Elemento encontrado
+    } else {
+        if(p==0){
+            if(lista->eFrMax<temp){
+                lista->eFrMax = temp;
+            }
+            lista->eFrCant++;
+            lista->costoEvoF+=temp;
+            lista->tempef+=lista->costoEvoF;
+            lista->eFrMed = lista->tempef/(lista->eFrCant);
+        }
+        *pos = (int)fin;
+        return 0; // Elemento no encontrado
+    }
 }
-
-
-
-
 
 int AltaLSOBB(lsobb *lista, Envio envio) {
 
 
-       lista->costo =0.0;
-    int pos;
+    lista->costo =0.0;
+    int pos=0;
 
     if (lista->contador == MAX_Envios) {
         return 2; // Lista llena
@@ -122,8 +131,8 @@ int AltaLSOBB(lsobb *lista, Envio envio) {
 
     if (LocalizarLSOBB(lista, envio.codigo, &pos,1)==0) {
 
-        for (int i = lista->contador-1; i >= pos; i--) {
-                   lista->costo++; //corrimiento
+        for (int i = lista->contador - 1; i >= pos; i--) {
+            lista->costo++; //corrimiento
 
 
             lista->envios[i + 1] = lista->envios[i];
@@ -133,20 +142,20 @@ int AltaLSOBB(lsobb *lista, Envio envio) {
 
 
 
-if (lista->costo > lista->aMax) {
+        if (lista->costo > lista->aMax) {
 
-    lista->aMax = lista->costo; //maximo
-
-
-
-}
+            lista->aMax = lista->costo; //maximo
 
 
-  lista->tempa+=lista->costo; //promedio
 
- lista->aMed=lista->tempa/(lista->aCant+1);
+        }
 
-lista->aCant++; //cantidad de altas
+
+        lista->tempa+=lista->costo; //promedio
+
+        lista->aMed=lista->tempa/(lista->aCant+1);
+
+        lista->aCant++; //cantidad de altas
 
         return 0;
     } else {
@@ -155,47 +164,99 @@ lista->aCant++; //cantidad de altas
     }
 }
 
+/*
+ *
+ * int AltaLSOBB(lsobb *lista, Envio envio) {
+
+
+    lista->costo =0.0;
+    int pos=0;
+
+    if (lista->contador == MAX_Envios) {
+        return 2; // Lista llena
+    }
+
+    if (LocalizarLSOBB(lista, envio.codigo, &pos,1)==0) {
+
+        for (int i = lista->contador - 1; i >= pos; i--) {
+
+
+
+            lista->envios[i + 1] = lista->envios[i];
+        }
+        lista->envios[pos] = envio;
+        lista->contador++;
+
+
+
+        return 0;
+    } else {
+
+        return 1;
+    }
+}
+ *
+ * int AltaLSOBB(lsobb *lista, Envio envio) {
+
+    lista->costo =0.0;
+    int pos;
+    if (lista->contador == MAX_Envios) {
+        return 2; // Lista llena
+    }
+    if (LocalizarLSOBB(lista, envio.codigo, &pos,1)==0) {
+        for (int i = lista->contador - 1; i >= pos; i--) {
+            lista->envios[i + 1] = lista->envios[i];
+        }
+        lista->envios[pos] = envio;
+        lista->contador++;
+        return 0;
+    } else {
+
+        return 1;
+    }
+}
+ */
 
 int BajaLSOBB(lsobb *lista, Envio envio) {
-       lista->costo =0.0;
+    lista->costo =0.0;
     int pos, i;
     int encontrado = LocalizarLSOBB(lista, envio.codigo, &pos,1);
 
     if( (strcmp(lista->envios[pos].direccion , envio.direccion)==0) && (lista->envios[pos].dni_receptor == envio.dni_receptor)
-           && (lista->envios[pos].dni_remitente == envio.dni_remitente) && (strcmp(lista->envios[pos].fecha_envio,envio.fecha_envio)==0)
-           && (strcmp(lista->envios[pos].fecha_recepcion,envio.fecha_recepcion)==0) && (strcmp(lista->envios[pos].nombre,envio.nombre)==0)
-           && (strcmp(lista->envios[pos].nombre_r,envio.nombre_r)==0)){
+        && (lista->envios[pos].dni_remitente == envio.dni_remitente) && (strcmp(lista->envios[pos].fecha_envio,envio.fecha_envio)==0)
+        && (strcmp(lista->envios[pos].fecha_recepcion,envio.fecha_recepcion)==0) && (strcmp(lista->envios[pos].nombre,envio.nombre)==0)
+        && (strcmp(lista->envios[pos].nombre_r,envio.nombre_r)==0)){
 
-    if (encontrado) {
-        for (i = pos; i < lista->contador - 1; i++) {
-                   lista->costo++; //corrimiento
-            lista->envios[i] = lista->envios[i + 1];
+        if (encontrado) {
+            for (i = pos; i < lista->contador - 1; i++) {
+                lista->costo++; //corrimiento
+                lista->envios[i] = lista->envios[i + 1];
+            }
+
+            pos--;
+            lista->contador--;
+
+
+
+            if (lista->costo > lista->bMax) {
+
+                lista->bMax = lista->costo; //maximo
+
+            }
+
+            lista->tempb+=lista->costo; //promedio
+
+
+            lista->bMed=lista->tempb/(lista->bCant+1);
+
+
+            lista->bCant++; //cantidad de bajas
+            return 0;
+        } else {
+            return 1;
         }
-
-           pos--;
-        lista->contador--;
-
-
-
-     if (lista->costo > lista->bMax) {
-
-    lista->bMax = lista->costo; //maximo
-
-}
-
-  lista->tempb+=lista->costo; //promedio
-
-
- lista->bMed=lista->tempb/(lista->bCant+1);
-
-
-lista->bCant++; //cantidad de bajas
-        return 0;
-    } else {
-        return 1;
     }
-           }
-           return 0;
+    return 0;
 }
 
 
@@ -205,7 +266,9 @@ lista->bCant++; //cantidad de bajas
 int evocarLSOBB(lsobb *lista, char codigo[], Envio *envio) {
     int pos;
     int encontrado = LocalizarLSOBB(lista, codigo, &pos,0);
+
     if (encontrado) {
+
         *envio = lista->envios[pos];
         return 1;
     } else {
--- cmake-build-debug/CMakeFiles/LSOBB_LIBT_ABB.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/LSOBB_LIBT_ABB.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/lautaro/OneDrive/Escritorio/LSOBB-LIBT-ABB/main.c" "CMakeFiles/LSOBB_LIBT_ABB.dir/main.c.obj" "gcc" "CMakeFiles/LSOBB_LIBT_ABB.dir/main.c.obj.d"
+  "C:/Users/Usuario/Downloads/LSOBB-LIBT-ABB/main.c" "CMakeFiles/LSOBB_LIBT_ABB.dir/main.c.obj" "gcc" "CMakeFiles/LSOBB_LIBT_ABB.dir/main.c.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
